a4ea35fbf79f7baef5f441448fb1a2c29d5b3230
Pedro Aurelio <xpedro.aurelio@gmail.com>
2026-06-11 20:18:38 -0300

    add home page with car search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,40 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import api from '../services/api'
 function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    const [cars, setCars] = useState([])
+    const [search, setSearch] = useState('')
+    const navigate = useNavigate()
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        try {
+            const response = await api.get('/cars?brand=' + search) 
+            setCars(response.data)
+        } catch (error) {
+            alert(error.response?.data?.message || 'Email ou senha incorretos')
+        }
+
+    }
+    return (
+        <div>
+            <h1>Home</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Buscar"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <button type="submit">Buscar</button>
+            </form>
+            {cars.map((car) => (
+                <div key={car.id}>
+                    <p>{car.brand} {car.model} {car.year}</p>
+                </div>
+            ))}
+        </div>
+    
   )
 }
 
